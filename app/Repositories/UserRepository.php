c0bfe245d4a648bbd18b56b27d87e9c8c158b344
dusan-dragas-vivify <dusan.dragas@vivifyideas.com>
2018-11-15 09:49:54 +0100

    Repository implemented

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vivify
+ * Date: 11/15/18
+ * Time: 9:48 AM
+ */
+
+namespace App\Repositories;
+
+use App\Interfaces\IUserRepository;
+use Illuminate\Http\Request;
+
+class UserRepository implements IUserRepository
+{
+
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function store(Request $request)
+    {
+        // TODO: Implement store() method.
+    }
+
+    public function show($id)
+    {
+        // TODO: Implement show() method.
+    }
+
+    public function edit($id)
+    {
+        // TODO: Implement edit() method.
+    }
+
+    public function update(Request $request, $id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function destroy($id)
+    {
+        // TODO: Implement destroy() method.
+    }
+}
