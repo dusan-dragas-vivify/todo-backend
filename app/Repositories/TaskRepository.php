9ce397bd24085c2879fac25898b82726311c332a
dusan-dragas-vivify <dusan.dragas@vivifyideas.com>
2018-11-19 09:23:04 +0100

    Controller and repo created

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vivify
+ * Date: 11/19/18
+ * Time: 9:16 AM
+ */
+
+namespace App\Repositories;
+
+
+use App\Interfaces\ITaskRepository;
+use Illuminate\Http\Request;
+
+class TaskRepository implements ITaskRepository
+{
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function store(Request $request)
+    {
+        // TODO: Implement store() method.
+    }
+
+    public function show($id)
+    {
+        // TODO: Implement show() method.
+    }
+
+    public function edit($id)
+    {
+        // TODO: Implement edit() method.
+    }
+
+    public function update(Request $request, $id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function destroy($id)
+    {
+        // TODO: Implement destroy() method.
+    }
+
+}
